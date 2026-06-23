b8cbe0f42e79a511f3ba61d958bae1c71598dbc6
Steffan Andrews <stef@orchetect.com>
2026-06-22 23:41:58 -0700

    Updated unit tests

---
+++ Tests/SwiftCoreAudioTests/Types/AudioUID/AudioUID+Properties Tests.swift
@@ -0,0 +1,26 @@
+//
+//  AudioUID+Properties Tests.swift
+//  SwiftCoreAudio • https://github.com/orchetect/swift-core-audio
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+import Foundation
+import Testing
+import SwiftCoreAudio
+
+@Suite struct AudioUID_Properties_Tests {
+    @Test
+    func isBlackHole() throws {
+        #expect(AudioUID<AnyAudioDevice>("BlackHole2ch_UID").isBlackHole)
+        #expect(AudioUID<AnyAudioDevice>("BlackHole16ch_UID").isBlackHole)
+        #expect(AudioUID<AnyAudioDevice>("BlackHole32ch_UID").isBlackHole)
+        #expect(AudioUID<AnyAudioDevice>("BlackHole64ch_UID").isBlackHole)
+        #expect(AudioUID<AnyAudioDevice>("BlackHole128ch_UID").isBlackHole)
+        #expect(AudioUID<AnyAudioDevice>("BlackHole256ch_UID").isBlackHole)
+
+        #expect(!AudioUID<AnyAudioDevice>("BlackHole").isBlackHole)
+        #expect(!AudioUID<AnyAudioDevice>("BlackHole_UID").isBlackHole)
+        #expect(!AudioUID<AnyAudioDevice>("BlackHole2Ch_UID").isBlackHole)
+        #expect(!AudioUID<AnyAudioDevice>("BlackHole2CH_UID").isBlackHole)
+    }
+}
