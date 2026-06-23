2f32c192ddddd4153d5f19572782deb6a37f9e37
Steffan Andrews <stef@orchetect.com>
2026-06-23 14:13:17 -0700

    `AudioChannelIndex`: Now conforms to Codable

--- Sources/SwiftCoreAudio/Types/ChannelIndex/AudioChannelIndex.swift
+++ Sources/SwiftCoreAudio/Types/ChannelIndex/AudioChannelIndex.swift
@@ -46,6 +46,8 @@ extension AudioChannelIndex: RawRepresentable {
     }
 }
 
+extension AudioChannelIndex: Codable { }
+
 extension AudioChannelIndex: Sendable { }
 
 extension AudioChannelIndex: CustomStringConvertible {
