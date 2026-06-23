2a20ba34e3f2f752794cfe943dd0615666a34f71
Steffan Andrews <stef@orchetect.com>
2026-06-22 21:54:10 -0700

    `AudioUID`: Added Codable conformance

--- Sources/SwiftCoreAudio/Types/AudioUID/AudioUID.swift
+++ Sources/SwiftCoreAudio/Types/AudioUID/AudioUID.swift
@@ -26,6 +26,19 @@ extension AudioUID: Equatable { }
 
 extension AudioUID: Hashable { }
 
+extension AudioUID: Codable {
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        self.init(string)
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
+}
+
 extension AudioUID: Sendable { }
 
 // MARK: - RawRepresentable
