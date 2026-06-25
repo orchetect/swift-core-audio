5d984567dd99b7ff8bb36b7451a664d3f9261567
Steffan Andrews <stef@orchetect.com>
2026-06-25 16:30:15 -0700

    `AudioSystemProperties`: `devices(with:)`: Added a `streamLookupErrorHandler` closure parameter to handle individual stream lookup errors

--- Sources/SwiftCoreAudio/Protocols/AudioSystemProperties/AudioSystemProperties+Devices.swift
+++ Sources/SwiftCoreAudio/Protocols/AudioSystemProperties/AudioSystemProperties+Devices.swift
@@ -29,14 +29,29 @@ extension AudioSystemProperties {
     /// Returns an array of devices currently available to the system that have at least one stream for the
     /// given direction.
     /// The order of devices in the array matches the order of devices as seen in Audio MIDI Setup.
+    ///
+    /// - Parameters:
+    ///   - direction: Input or output audio stream direction.
+    ///   - streamLookupErrorHandler: Optionally supply an error handler that will be called for any UIDs that fail lookup.
+    ///     If this closure is `nil`, failures are silently ignored but will still be logged.
+    /// - Throws: Throws an error if device enumeration fails. Individual failures on a per-device basis are
+    ///   passed to the `streamLookupErrorHandler` closure and do not cause this method itself to throw.
     nonisolated
-    public func devices(with direction: AudioStream.Direction) throws(SwiftCoreAudioError) -> [AnyAudioDevice] {
+    public func devices(
+        with direction: AudioStream.Direction,
+        streamLookupErrorHandler: ((_ device: AnyAudioDevice, _ error: SwiftCoreAudioError) -> ())? = nil
+    ) throws(SwiftCoreAudioError) -> [AnyAudioDevice] {
         let allDevices = try devices
 
         var includedDevices: [AnyAudioDevice] = []
         for device in allDevices {
-            if try device.hasStreams(for: direction) {
-                includedDevices.append(device)
+            do throws(SwiftCoreAudioError) {
+                if try device.hasStreams(for: direction) {
+                    includedDevices.append(device)
+                }
+            } catch {
+                Logging.log(.error, "Error looking up \(direction) stream information for audio device with ID \(device.id): \(error)")
+                streamLookupErrorHandler?(device, error)
             }
         }
 
