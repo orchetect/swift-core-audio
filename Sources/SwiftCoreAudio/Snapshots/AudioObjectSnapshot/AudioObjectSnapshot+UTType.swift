d2ee50444c65e10a505e209f1c746a4ae40dc85f
Steffan Andrews <stef@orchetect.com>
2026-08-12 20:32:24 -0700

    `UTType`: `coreAudioSnapshot` static property: Changed from export to import

--- Sources/SwiftCoreAudio/Snapshots/AudioObjectSnapshot/AudioObjectSnapshot+UTType.swift
+++ Sources/SwiftCoreAudio/Snapshots/AudioObjectSnapshot/AudioObjectSnapshot+UTType.swift
@@ -12,7 +12,7 @@ import Foundation
 @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 extension UTType {
     /// UT Type for ``AudioObjectSnapshot``'s encoded data format, with filename extension `coreaudiosnapshot`.
-    public static let coreAudioSnapshot = UTType(exportedAs: "com.orchetect.SwiftCoreAudio.Snapshot")
+    public static let coreAudioSnapshot = UTType(importedAs: "com.orchetect.SwiftCoreAudio.Snapshot")
 }
 
 #endif
